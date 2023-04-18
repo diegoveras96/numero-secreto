7e77873ecb1a0e72084daa83df2b0a1e29b06936
Diego Veras <diego.veras96@hotmail.com>
2023-04-18 17:47:27 -0300

    Update sortearNumero.js

--- js/sortearNumero.js
+++ js/sortearNumero.js
@@ -3,29 +3,53 @@ window.addEventListener("contextmenu", (event)=>{
 })
 const btn = document.querySelector('#btn-comeco')
 var minValor = 1;
-const maxValor = 100;
+var maxValor = 100;
 
-const elementoMenor = document.querySelector('#menor-valor');
-elementoMenor.innerHTML = minValor;
+const elementoNumero = document.querySelectorAll('.numero');
+elementoNumero[0].innerHTML = minValor;
+elementoNumero[1].innerHTML = maxValor;
 
-const elementoMaior = document.querySelector("#maior-valor");
-elementoMaior.innerHTML = maxValor;
-
-elementoMenor.addEventListener("mousedown", (click)=>{
-    console.log(click)
-    if (click.button == 0){
-        minValor++;
-    } else if(click.button == 2){
-        if (minValor <= 0){
-            minValor = 0;
-        } else{
-            minValor--;
-        }
-    }
-    elementoMenor.innerHTML = minValor;
+elementoNumero.forEach((elemento)=>{
+    elemento.addEventListener("mousedown", (valor)=>{
+        operacao(valor.target.id, valor.target.innerHTML, valor.button);
+    })
 })
 
 btn.addEventListener("click", ()=>{
-    const numeroSecreto = parseInt(Math.random()* maxValor + minValor);
+    if(minValor > maxValor){ //Verificação para caso do minValor seja maior que o maxValor. Se isso acontecer, o código irá trocar os valores para poder funcionar corretamente
+        let aux = minValor;
+        minValor = maxValor;
+        maxValor = aux;
+    }
+    const numeroSecreto = Math.floor(Math.random() * (maxValor - minValor + 1) + minValor); //Função que pega o inteiro entre os números escolhidos
     console.log(numeroSecreto)
 })
+
+function operacao(id, valor, mouse){
+
+    //Pega o botão do Mouse que aconteceu o Click e altera a variável auxiliar $valor, usada porque ainda não sabemos qual estamos mexendo
+    if (mouse === 0){
+        valor++;
+    } else if(mouse == 2){
+        valor--;
+    }
+
+    //Pega o ID para alterar a variável correta
+    if (id.includes("maior")){
+        maxValor = valor;
+        trocarHTML(1, maxValor);
+    } else if (id.includes("menor")){
+        minValor = valor;
+        if (minValor < 0){
+            minValor = 0;
+        }
+        trocarHTML(0, minValor);
+    }
+    
+}
+
+//Função para trocar o HTML da página
+function trocarHTML(i, valor){
+    elementoNumero[i].innerHTML = valor;
+    elementoNumero[i].innerHTML = valor;
+}
